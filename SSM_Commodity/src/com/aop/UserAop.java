5202b73f423257441b8f045e020cfdf62a355e84
DELL <DELL@DESKTOP-IF9CJNG>
2019-03-05 16:06:42 +0800

    aop修改

--- SSM_Commodity/src/com/aop/UserAop.java
+++ SSM_Commodity/src/com/aop/UserAop.java
@@ -27,15 +27,21 @@ public class UserAop {
 	 */
 	@Before("execution(* com.service.impl.*.*(..))")
 	public void before(JoinPoint jp){
-		log.info("===============================================调用："+jp.getTarget()+"的"
-				+jp.getSignature().getName()+"方法。==============方法入参："+Arrays.toString(jp.getArgs())+"=======================================");
+		log.info("================================前置增强方法============================");
+		log.info("调用："+jp.getTarget()+"的"+jp.getSignature().getName()+"方法。" );
+		log.info("=================================前置增强方法===========================");
+		log.info("方法入参："+Arrays.toString(jp.getArgs()));
+		log.info("=================================前置增强方法===========================");
 	}
 	 
 	//后置增强方法
 	@AfterReturning(pointcut="execution(* com.service.impl.*.*(..))",returning="result")
 	public void after(JoinPoint jp,Object result){
-		log.info("==============================================调用："+jp.getTarget()+"的"
-				+jp.getSignature().getName()+"方法。=============返回值："+result+"=======================================");
+		log.info("================================后置增强方法============================");
+		log.info("调用："+jp.getTarget()+"的"+jp.getSignature().getName()+"方法。");
+		log.info("================================后置增强方法============================");
+		log.info("=============返回值："+result+"=======================================");
+		log.info("================================后置增强方法============================");
 	}
 	
 
